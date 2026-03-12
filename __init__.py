c02dbd5e99d277eb0c9681a3aafd2ed5d27c70b7
0xfbad <106453412+0xfbad@users.noreply.github.com>
2026-03-12 11:55:31 -0700

    format __init__.py

--- __init__.py
+++ __init__.py
@@ -9,22 +9,20 @@ from .utils import settings_to_dict
 from .container_manager import ContainerManager
 from .views import containers_bp
 
+
 def load(app: Flask):
-	app.db.create_all()
-	CHALLENGE_CLASSES["container"] = ContainerChallenge
+    app.db.create_all()
+    CHALLENGE_CLASSES["container"] = ContainerChallenge
 
-	plugin_name = os.path.basename(os.path.dirname(__file__))
-	assets_path = f"plugins/{plugin_name}/assets"
-	register_plugin_assets_directory(app, base_path=assets_path)
+    plugin_name = os.path.basename(os.path.dirname(__file__))
+    assets_path = f"plugins/{plugin_name}/assets"
+    register_plugin_assets_directory(app, base_path=assets_path)
 
-	container_settings = settings_to_dict(ContainerSettingsModel.query.all())
-	container_manager = ContainerManager(container_settings, app)
+    container_settings = settings_to_dict(ContainerSettingsModel.query.all())
+    container_manager = ContainerManager(container_settings, app)
 
-	app.container_manager = container_manager
+    app.container_manager = container_manager
 
-	app.register_blueprint(containers_bp)
+    app.register_blueprint(containers_bp)
 
-	register_admin_plugin_menu_bar(
-		title="Containers",
-		route="/containers/dashboard"
-	)
+    register_admin_plugin_menu_bar(title="Containers", route="/containers/dashboard")
