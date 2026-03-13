577846f4d169d306f410b687cb5aa04445080256
0xfbad <106453412+0xfbad@users.noreply.github.com>
2026-03-12 22:11:53 -0700

    add pub_hostname column to docker context model

--- models.py
+++ models.py
@@ -47,5 +47,6 @@ class DockerContextModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     context_name = db.Column(db.String(512), unique=True, nullable=False)
     hostname = db.Column(db.String(512), nullable=True)
+    pub_hostname = db.Column(db.String(512), nullable=True)
     weight = db.Column(db.Integer, default=1)
     enabled = db.Column(db.Boolean, default=True)
